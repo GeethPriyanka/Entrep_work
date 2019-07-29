bcbd265433c2e8e4f8ee51535ac6c146e0c1d21b
Geeth <geeth.priyanka@yahoo.com>
2019-07-29 19:43:59 +0530

    conversion to php is done

---
+++ Website/includes/header.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from demo.graygrids.com/themes/engage-stable/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 30 Apr 2018 06:03:01 GMT -->
+<head>
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		Engage - Multi-Purose Bootstrap HTML5 Template
+	</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/color-switcher.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+
+	<link rel="stylesheet" media="screen" href="assets/fonts/font-awesome/font-awesome.min.css">
+	<link rel="stylesheet" media="screen" href="assets/fonts/simple-line-icons.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/extras/owl/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="assets/extras/owl/owl.theme.css">
+	<link rel="stylesheet" type="text/css" href="assets/extras/animate.css">
+	<link rel="stylesheet" type="text/css" href="assets/extras/normalize.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/colors/green.css" media="screen" />
+
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js">
+    </script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js">
+    </script>
+<![endif]-->
+</head>
+<body>
+
+	<header id="header-wrap">
+
+		<nav class="navbar navbar-expand-md fixed-top scrolling-navbar nav-bg">
+			<div class="container">
+
+				<div class="navbar-header">
+					<a class="navbar-brand" href="index.html">
+						<img src="assets/img/logo.png" alt="">
+					</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse" id="main-menu">
+					<ul class="navbar-nav justify-content-end">
+						<li class="nav-item">
+							<a class="nav-link active" href="index-edited.php" >
+								Home
+							</a>
+						</li>
+						<li>
+							<a class="nav-link" href="">
+								සිංහල
+							</a>
+						</li>
+						<li>
+							<a class="nav-link" href="">
+								தமிழ்
+							</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contact Us</a>
+<!--							<div class="dropdown-menu">-->
+<!--								<a class="dropdown-item" href="contact1.html">Contact us 1</a>-->
+<!--								<a class="dropdown-item" href="contact2.html">Contact us 2</a>-->
+<!--							</div>-->
+						</li>
+					</ul>
+					<form class="form-inline">
+						<div class="top_search_con">
+							<input class=" mr-sm-2" type="text" placeholder="Search Here ...">
+							<span class="top_search_icon"><i class="icon-magnifier"></i></span>
+						</div>
+					</form>
+					<ul class="navbar-nav ml-auto" id="navbar-login">
+						<li class="nav-item">
+							<a class="nav-link active" href="#" >
+								Sign-up
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link active" href="#" >
+								Login
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<ul class="wpb-mobile-menu">
+					<li>
+						<a class="active" href="index.html">Home</a>
+					</li>
+                    <li>
+                        <a class="nav-link" href="">
+                            සිංහල
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="">
+                            தமிழ்
+                        </a>
+                    </li>
+					<li>
+						<a href="#">Contact Us</a>
+						<ul>
+							<li><a href="contact1.html">Contact us 1</a></li>
+							<li><a href="contact2.html">Contact us 2</a></li>
+						</ul>
+					</li>
+				</ul>
+
+			</div>
+        </nav>
+</header>
---
+++ Website/want-to-work.php
@@ -0,0 +1,27 @@
+<?php include('includes/header.php');?>
+
+<div class="row" id="wanttowork-container">
+	<div class="col-md-2">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore iste, sint nesciunt nobis 
+			adipisci fugiat corrupti dolores ex magnam voluptas rem illum porro. Aliquam distinctio hic 
+			voluptatem assumenda eligendi. Veniam.</p>
+	</div>
+
+	<div class="col-md-8">
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore iste, sint nesciunt nobis 
+			adipisci fugiat corrupti dolores ex magnam voluptas rem illum porro. Aliquam distinctio hic 
+			voluptatem assumenda eligendi. Veniam.</p>
+	</div>
+
+	<div class="col-md-2">
+
+	</div>
+
+</div>
+		
+
+
+	<?php include('includes/footer.php');?>
+
+
+</html>
